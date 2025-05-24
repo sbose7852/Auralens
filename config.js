80ac8221d82f7f954f952dc2ad54c41af5db5582
sbose7852 <sbose7852@gmail.com>
2025-05-24 11:28:31 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// AuraLens/config.js
+
+// !!! IMPORTANT !!!
+// 1. DO NOT COMMIT THIS FILE TO GIT if it contains your real API key.
+// 2. Add 'config.js' to your .gitignore file.
+// 3. Replace "YOUR_NEW_SECURE_GEMINI_API_KEY_HERE" with your actual new API key.
+
+const GEMINI_API_KEY = "YOUR_NEW_SECURE_GEMINI_API_KEY_HERE";
+
+// You can export it if your background script is a module,
+// or it will be available globally if imported as a non-module script.
+// For simplicity with service workers, we'll make it globally available
+// by importing it first in the manifest.
