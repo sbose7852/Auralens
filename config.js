27dda2a703b353a6f7c30fe41b38614f2b69c861
sbose7852 <sbose7852@gmail.com>
2025-05-24 11:31:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,4 @@
 // 2. Add 'config.js' to your .gitignore file.
 // 3. Replace "YOUR_NEW_SECURE_GEMINI_API_KEY_HERE" with your actual new API key.
 
-const GEMINI_API_KEY = "YOUR_NEW_SECURE_GEMINI_API_KEY_HERE";
-
-// You can export it if your background script is a module,
-// or it will be available globally if imported as a non-module script.
-// For simplicity with service workers, we'll make it globally available
-// by importing it first in the manifest.
+export const GEMINI_API_KEY = "YOUR_NEW_SECURE_GEMINI_API_KEY_HERE";
